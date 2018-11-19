a7129f62b5423933014ecb07e5540565a556ce2a
SHEIK ABDULLAH P <sheikp@gmail.com>
2018-11-20 01:37:43 +0530

    Create funTest.js

---
+++ funTest.js
@@ -0,0 +1,89 @@
+CTF Thrones
+-------------------
+Flags:
+Dorne Flag: fb8d98be1265dd88bac522e1b2182140
+Winterfell Flag: 639bae9ac6b3e1a84cebb7b403297b79
+Iron Islands Flag: 5e93de3efa544e85dcd6311732d28f95
+Stormlands Flag: 8fc42c6ddf9966db3b09e84365034357
+Kingdom of the Mountain and the Vale Flag: bb3aec0fdcdbc2974890f805c585d432
+Kingdom of Reach Flag: aee750c2009723355e2ac57564f9c3db
+King’s Landing Flag: c8d46d341bea4fd5bff866a65ff8aea9
+Savages Flag: 8bf8854bebe108183caeb845c7676ae4
+City of Braavos Flag: 3f82c41a70a8b0cfec9052252d9fd721
+DragonGlass Mine Flag: a8db1d82db78ed452ba0882fb9554fc9
+Final Battle Flag: 8e63dcd86ef9574181a9b6184ed3dde5
+
+Docker
+----------
+2aa11783d05b6a329ffc4d2a1ce037f46162253e55d53764a6a7e998
+d867a73c70770e73b65e6949dd074285dfdee80a8db333a7528390f6
+
+Android
+-----------
+ANDROID{u GOT root buddy}
+
+Reverse Engg.
+-------------------
+Flag of the Cow(Until the Cows come home 
+I shouldve used dynamic link
+
+FlagOfTheBear(Unbearable Revenge) 
+They couldnt bear the sight of you
+
+FlagOfTheOverachiever(Overachiever)
+Achievement Unlocked Red Ring of Death
+
+FlagOfTheEggHunter(Egghunter)
+The fortress of Anorak is all yours
+
+FlagOfThePirate(Pirate's Treasure)
+Avast! Ya got my arrrr es eh
+
+FlagOfTheLava(Fire and Ice)
+Some bosses just roll over and die
+
+FlagOfTheBlock(Blocky's Revenge)
+It may glow red but its not stone
+
+
+Web
+------
+inform vulnerable library 
+contact us -> sequelize 1.7 
+
+inform algorithm - 
+contac us -> z85 0.0
+
+coupon 80% discount
+pEw8ohz3{y
+
+cryptanalysis to find easter egg
+/the/devs/are/so/funny/they/hid/an/easter/egg/within/the/easter/egg
+
+upload file without pdf
+use burp and change the extension
+
+upload file larger than 100KB
+use burp and add more content and forward
+
+change the description
+postman -> PUT /api/Products/8 
+
+Tier2 XSS
+user registration -> burp suite change the email with alert with escaping"
+
+Tier3 XSS
+Contact us -> 
+<<script>alert("XSS3")</script>script>alert("XSS3")<</script>/script>
+
+Tier4 XSS
+/api/Products/7
+{"description":  "<script>alert(\"XSS4\")</script>"}
+
+Wherever you go, there you are
+/redirect?to=/%00https://github.com/bkimminich/juice-shop
+
+Christmas special offer
+Search textbox payload -> christmas%25'))--
+add to basket and checkout
+
